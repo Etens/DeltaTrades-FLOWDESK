c311a44c75e97ae4e7c12f38fbd923ed8d95cd8a
Etens <45323562+Etens@users.noreply.github.com>
2023-05-10 08:51:27 +0300

    Completed feature implementation

--- src/middlewares/deltaMiddleware .ts
+++ src/middlewares/deltaMiddleware .ts
@@ -1,13 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { Trade } from '../models/trade';
 import { KucoinTradeHistoryAPI } from '../services/tradeHistoryKucoin';
-import { BinanceTradeHistoryAPI } from '../services/tradeHistoryBinance'; 
 
 let deltaCumulative = 0;
 
 export async function deltaMiddleware(req: Request, res: Response, next: NextFunction) {
   const exchange = req.params.exchange;
   const tradingPair = req.params.tradingPair;
+  exchange.toUpperCase();
   
   let trades: Trade[] = [];
   let api;
@@ -15,14 +15,10 @@ export async function deltaMiddleware(req: Request, res: Response, next: NextFun
   if (exchange === 'kucoin') {
     api = new KucoinTradeHistoryAPI();
     trades = await api.getPublicTradeHistory(tradingPair);
-  } else if (exchange === 'binance') {
-    api = new BinanceTradeHistoryAPI();
-    trades = await api.getPublicTradeHistory(tradingPair);
+    // --- ADD HERE NEW EXCHANGE ---
   } else {
     console.error(`Unknown exchange: ${exchange}`);
   }
-
-  console.log(`Received ${trades.length} trades from ${exchange}`);
 
   for (const trade of trades) {
     if (trade.side === 'buy') {
--- src/routes/flowdeskRoutes.ts
+++ src/routes/flowdeskRoutes.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { TradeHistoryAPI } from '../services/tradeHistoryAPI';
 import { deltaMiddleware } from '../middlewares/deltaMiddleware ';
 import { KucoinTradeHistoryAPI } from '../services/tradeHistoryKucoin';
-import { BinanceTradeHistoryAPI } from '../services/tradeHistoryBinance'; 
 
 const router = express.Router();
 
@@ -10,38 +9,35 @@ function getExchangeAPI(exchangeName: string): TradeHistoryAPI {
   switch (exchangeName.toUpperCase()) {
     case 'KUCOIN':
       return new KucoinTradeHistoryAPI();
-    case 'BINANCE':
-      return new BinanceTradeHistoryAPI();
-    // Add more exchanges here
+    // --- ADD HERE NEW EXCHANGE --
     default:
       throw new Error(`Unsupported exchange: ${exchangeName}`);
   }
 }
 
-async function getTrades(api: TradeHistoryAPI, tradingPair: string) {
-  const trades = await api.getPublicTradeHistory('exchange name', tradingPair);
+async function getTrades(api: TradeHistoryAPI, exchangeName: string, tradingPair: string) {
+  const trades = await api.getPublicTradeHistory(exchangeName, tradingPair);
   return trades;
 }
 
 router.get('/trades/:exchange/:tradingPair', async (req, res, next) => { 
   try {
+    const exchangeName = req.params.exchange.toUpperCase();
     const tradingPair = req.params.tradingPair.toUpperCase();
-    const exchange = req.params.exchange.toUpperCase();
-
-    const api = getExchangeAPI(exchange);
-
-    const trades = await getTrades(api, tradingPair);
-
+    const api = getExchangeAPI(exchangeName);
+    const trades = await getTrades(api, exchangeName, tradingPair);
     res.locals.trades = trades;
     next();
   } catch (error) {
     next(error);
   }
 }, deltaMiddleware, (req, res) => {
+  const exchangeName = req.params.exchange.toUpperCase();
   const tradingPair = req.params.tradingPair.toUpperCase();
   const delta = res.locals.delta;
 
   res.status(200).json({
+    exchange: exchangeName,
     pair: tradingPair,
     delta: delta,
   });
